65e3d4c98a496161c4548e02cbab122d8038eb8e
AlkTirq <quayeturquoise@gmail.com>
2023-08-11 19:52:47 +0000

    Low level Programming Begins

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - puts coding method
+* 
+* Return : 0
+*/
+
+int main(viod)
+{
+            puts("Programming is like building a multilingual puzzle");
+            return (0);
+}
