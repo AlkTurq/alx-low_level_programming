e20ffaa53ec3687a6648077bb3334c7bfa975464
AlkTurq <quayedanturquoise@gmail.com>
2023-08-14 19:04:52 +0000

    betty corrections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
-* main - using printf
-* 
-* Return : 0
+* main - printing line of text
+* Return : Return: Always 0 (Success)
 */
 
 int main(viod)
 {
-            printf("with proper grammar, but the outcome is a piece of art,");
-            return (0);
-}
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+i}
