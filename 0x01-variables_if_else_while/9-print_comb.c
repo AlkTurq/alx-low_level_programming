62351224c455048af497ac7089e64ffa2a4d9694
AlkTirq <quayeturquoise@gmail.com>
2023-08-14 23:49:31 +0000

    corrections

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,18 +12,10 @@ int main(void)
 	for (; num < 10; num++)
 	{
 		putchar(num);
-		putchar#include <stdio.h>
+		putchar(',');
+	}
 
-			/**
-			 * main - program that prints all possible combinations of single-digit numbers
-			 * Return: Always 0 (Success)
-			 */
+	putchar('\n');
 
-			int main(void)
-			{
-				int num = 0;
-
-				for (; num < 10; num++)
-				{
-					putchar(num);
-					putcr
+	return (0);
+}
