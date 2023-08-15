c7587ad9d19ff3d5ffc6f1b18ff6a2ef3f241363
AlkTurq <quayedanturquoise@gmail.com>
2023-08-15 01:23:20 +0000

    loops

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main -  prints all possible different combinations of two digits.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num1 = '0';
+	int num2;
+
+	for (; num1 < '9'; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+
+			if (num1 == '8' && num2 == '9')
+			{
+				continue;
+			}
+
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,17 @@
 
 int main(void)
 {
-	int num = 0;
+	int num = '0';
 
-	for (; num < 10; num++)
+	for (; num <= '9'; num++)
 	{
 		putchar(num);
-		putchar(',');
+
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
